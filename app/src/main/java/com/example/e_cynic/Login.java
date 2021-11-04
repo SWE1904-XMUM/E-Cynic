72ad539ab7c5a635fc5d924c6d4dd2b9989471ec
pjou00 <peejou90@gmail.com>
2021-11-04 22:19:46 +0800

    Create forgot password page.

--- app/src/main/java/com/example/e_cynic/Login.java
+++ app/src/main/java/com/example/e_cynic/Login.java
@@ -12,6 +12,7 @@ public class Login extends AppCompatActivity
 {
     private Button loginBtn;
     private Button signUpBtn;
+    private Button forgotPwd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -21,6 +22,7 @@ public class Login extends AppCompatActivity
 
         loginBtn = findViewById(R.id.loginBtn);
         signUpBtn = findViewById(R.id.signUpBtn);
+        forgotPwd = findViewById(R.id.forgotPwd);
 
         loginBtn.setOnClickListener(new View.OnClickListener()
         {
@@ -28,6 +30,26 @@ public class Login extends AppCompatActivity
             public void onClick(View view)
             {
                 Intent i = new Intent(Login.this,Home.class);
+                startActivity(i);
+            }
+        });
+
+        signUpBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(Login.this,SignUp.class);
+                startActivity(i);
+            }
+        });
+
+        forgotPwd.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(Login.this,ForgotPassword.class);
                 startActivity(i);
             }
         });
