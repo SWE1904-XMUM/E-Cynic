d7b32d37ee6e40474a086b2ef0cc11519027ba17
pjou00 <peejou90@gmail.com>
2021-11-04 23:04:42 +0800

    Add about E-Cynic page.

--- app/src/main/java/com/example/e_cynic/Profile.java
+++ app/src/main/java/com/example/e_cynic/Profile.java
@@ -5,17 +5,34 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Profile extends AppCompatActivity
 {
+    private Button about;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
+        about = findViewById(R.id.about);
+
         bottomNavBar();
+
+        about.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(Profile.this,About.class);
+                startActivity(i);
+            }
+        });
     }
 
     public void bottomNavBar()
