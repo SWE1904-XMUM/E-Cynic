cdb91ea3e9699c7ddeb1fe90de42315374355e50
gq <swe1904862@xmu.edu.my>
2021-11-11 23:59:10 +0800

    edit colours and add StartupActivity (first screen)

---
+++ app/src/main/java/com/example/e_cynic/activity/StartupActivity.java
@@ -0,0 +1,27 @@
+package com.example.e_cynic.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.e_cynic.db.DatabaseConnectionProvider;
+
+public class StartupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //create database connection on startup
+        DatabaseConnectionProvider.getConnection(this.getApplicationContext());
+
+        //direct to LoginActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(StartupActivity.this, LoginActivity.class));
+            }
+        },0);
+    }
+}
