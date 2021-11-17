478800d4de519e535b6b06c4fd9a2f375b2b469d
Yyin <yinyinteo0720@gmail.com>
2021-11-17 09:13:40 +0800

    Spinner On SignUp

--- app/src/main/java/com/example/e_cynic/activity/SignUpActivity.java
+++ app/src/main/java/com/example/e_cynic/activity/SignUpActivity.java
@@ -2,6 +2,9 @@ package com.example.e_cynic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
 import com.example.e_cynic.R;
 
 public class SignUpActivity extends AppCompatActivity
@@ -11,5 +14,12 @@ public class SignUpActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<String> myAdapter=new ArrayAdapter<String>(SignUpActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.state));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
 }
