493fbbf4efbb90f80aa3af907c77585fcd6d284d
Yyin <yinyinteo0720@gmail.com>
2021-11-21 01:22:21 +0800

    Reward Page

--- app/src/main/java/com/example/e_cynic/activity/RedeemPointsActivity.java
+++ app/src/main/java/com/example/e_cynic/activity/RedeemPointsActivity.java
@@ -1,15 +1,35 @@
 package com.example.e_cynic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import com.example.e_cynic.R;
 
 public class RedeemPointsActivity extends AppCompatActivity
 {
+    private ImageView backBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.redeem_points);
+
+        backBtn = findViewById(R.id.backBtn);
+
+        backBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(RedeemPointsActivity.this,
+                        HistoryActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
