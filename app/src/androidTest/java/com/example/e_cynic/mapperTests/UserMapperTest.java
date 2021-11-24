8ec862b0afb61f2c5cf17c97a43823d05743c36c
gq <swe1904862@xmu.edu.my>
2021-11-24 23:02:09 +0800

    improve UserDatabase

---
+++ app/src/androidTest/java/com/example/e_cynic/mapperTests/UserMapperTest.java
@@ -0,0 +1,44 @@
+package com.example.e_cynic.mapperTests;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.example.e_cynic.db.mapper.UserMapper;
+import com.example.e_cynic.entity.User;
+import com.example.e_cynic.utils.GetDatabaseUtil;
+import com.example.e_cynic.utils.LoggingUtil;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserMapperTest {
+
+    SQLiteDatabase database = GetDatabaseUtil.getTestDatabase();
+
+    @Test
+    public void mapToOneUser_test() throws InvocationTargetException, NoSuchMethodException,
+            NoSuchFieldException, InstantiationException, IllegalAccessException {
+        Cursor cursor = database.rawQuery("select * from users limit 1", null);
+        User user = null;
+        if (cursor.moveToNext()) {
+            user = UserMapper.mapCursorToOneUser(cursor);
+        }
+        LoggingUtil.printMessage("map to one user", user.toString());
+    }
+
+    @Test
+    public void mapToManyUsers_test() throws InvocationTargetException, NoSuchMethodException, NoSuchFieldException, InstantiationException, IllegalAccessException {
+        Cursor cursor = database.rawQuery("select * from users", null);
+        List<User> userList = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            userList.add(UserMapper.mapCursorToOneUser(cursor));
+        }
+
+        for (User u : userList) {
+            LoggingUtil.printMessage("map to many users", u.toString());
+        }
+    }
+}
