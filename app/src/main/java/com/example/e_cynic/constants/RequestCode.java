00780bc1ef523b907742aa0974835fab77db9708
pjou00 <peejou90@gmail.com>
2021-11-25 13:02:10 +0800

    Create FetchAddressService.

--- app/src/main/java/com/example/e_cynic/constants/RequestCode.java
+++ app/src/main/java/com/example/e_cynic/constants/RequestCode.java
@@ -3,10 +3,10 @@ package com.example.e_cynic.constants;
 public class RequestCode
 {
     // Request code
-    public static final int SNAP_PHOTO = 0;
-    public static final int CHOOSE_FROM_GALLERY = 1;
-    public static final int PHOTO_PERMISSION = 2;
-    public static final int LOCATION_PERMISSION = 3;
+    public static final int SNAP_PHOTO = 10;
+    public static final int CHOOSE_FROM_GALLERY = 11;
+    public static final int PHOTO_PERMISSION = 12;
+    public static final int LOCATION_PERMISSION = 13;
 
     RequestCode()
     {
