596eb1ca3bbdd771ae653578c67bd6ac1ebc2ccc
gq <swe1904862@xmu.edu.my>
2021-11-25 19:06:45 +0800

    get address from latitude and longitude

--- app/src/main/java/com/example/e_cynic/activity/PinLocationActivity.java
+++ app/src/main/java/com/example/e_cynic/activity/PinLocationActivity.java
@@ -2,6 +2,7 @@ package com.example.e_cynic.activity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Location;
 import android.os.Bundle;
 
@@ -12,6 +13,7 @@ import androidx.fragment.app.FragmentActivity;
 import com.example.e_cynic.R;
 import com.example.e_cynic.constants.RequestCode;
 import com.example.e_cynic.permission.LocationPermission;
+import com.example.e_cynic.utils.LocationUtil;
 import com.example.e_cynic.utils.userInteraction.ToastCreator;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -20,9 +22,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
 
 public class PinLocationActivity extends FragmentActivity implements OnMapReadyCallback
 {
@@ -72,10 +77,36 @@ public class PinLocationActivity extends FragmentActivity implements OnMapReadyC
     public void onMapReady(@NonNull GoogleMap googleMap)
     {
         LatLng latLng = new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude());
-        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("Your location");
+        MarkerOptions markerOptions =
+                new MarkerOptions().position(latLng).title("Your location").draggable(true);
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,5));
         googleMap.addMarker(markerOptions);
+
+        googleMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDrag(@NonNull Marker marker) {
+            }
+
+            @Override
+            public void onMarkerDragEnd(@NonNull Marker marker) {
+                try {
+                    LatLng newLatLng = new LatLng(marker.getPosition().latitude,
+                            marker.getPosition().longitude);
+                    Address address = LocationUtil.getAddressByLongitudeLatitude(PinLocationActivity.this
+                            , newLatLng.longitude, newLatLng.latitude);
+                    if(address != null) {
+                        //TODO do something with the address
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onMarkerDragStart(@NonNull Marker marker) {
+            }
+        });
     }
 
     @Override
