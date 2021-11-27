d9f2ee02a14229c80e07d94d5ae0bfad7f901f19
pjou00 <peejou90@gmail.com>
2021-11-27 16:06:27 +0800

    Create ArrayList package & class.

---
+++ app/src/main/java/com/example/e_cynic/arrayLists/HistoryArrayList.java
@@ -0,0 +1,16 @@
+package com.example.e_cynic.arrayLists;
+
+import java.util.ArrayList;
+
+public class HistoryArrayList
+{
+    ArrayList<String> itemName = new ArrayList<>();
+    ArrayList<String> numberOfItems = new ArrayList<>();
+    ArrayList<String> price = new ArrayList<>();
+    ArrayList<String> date = new ArrayList<>();
+
+    public void storeItemInArray()
+    {
+        // TODO -> connect to database
+    }
+}
