86cb234bd318e2de36d73da7ff6c83a9d7496c20
pjou00 <peejou90@gmail.com>
2021-11-27 17:59:43 +0800

    Delete articles table + add PointsDatabase & PointsMapper.

---
+++ app/src/main/java/com/example/e_cynic/db/PointsDatabase.java
@@ -0,0 +1,14 @@
+package com.example.e_cynic.db;
+
+import android.database.sqlite.SQLiteDatabase;
+
+public class PointsDatabase
+{
+    public static final String pointsTable = "points";
+    public static final String userId = "userId";
+    public static final String pointsEarned = "pointsEarned";
+    public static final String date = "date";
+
+    private static SQLiteDatabase db = DatabaseConnectionProvider.getDatabase(null);
+
+}
