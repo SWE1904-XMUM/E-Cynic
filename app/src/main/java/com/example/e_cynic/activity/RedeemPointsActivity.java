7a04ca6801d89945bf1d2e7f50bacba4a3e2fe97
pjou00 <peejou90@gmail.com>
2021-12-02 11:48:51 +0800

    Update RedeemPointsActivity with session manager.

--- app/src/main/java/com/example/e_cynic/activity/RedeemPointsActivity.java
+++ app/src/main/java/com/example/e_cynic/activity/RedeemPointsActivity.java
@@ -1,17 +1,21 @@
 package com.example.e_cynic.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.e_cynic.R;
+import com.example.e_cynic.session.SessionManager;
 
 public class RedeemPointsActivity extends AppCompatActivity
 {
     private ImageView backBtn;
+    private TextView availablePoints;
+    SessionManager sm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,7 +23,9 @@ public class RedeemPointsActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.redeem_points);
 
-        backBtn = findViewById(R.id.backBtn);
+        sm = new SessionManager(getApplicationContext());
+        setViewComponent();
+        setAvailablePoints();
 
         backBtn.setOnClickListener(new View.OnClickListener()
         {
@@ -31,5 +37,17 @@ public class RedeemPointsActivity extends AppCompatActivity
                 startActivity(i);
             }
         });
+    }
+
+    private void setViewComponent()
+    {
+        backBtn = findViewById(R.id.backBtn);
+        availablePoints = findViewById(R.id.availablePoints);
+    }
+
+    private void setAvailablePoints()
+    {
+        int p = sm.getTotalPoints();
+        availablePoints.setText(String.valueOf(p));
     }
 }
