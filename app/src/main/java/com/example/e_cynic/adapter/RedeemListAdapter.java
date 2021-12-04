49267da182c3d4ec51a003f0dfdbaaac22d64c34
pjou00 <peejou90@gmail.com>
2021-12-04 22:50:03 +0800

    Done redeem points page.

--- app/src/main/java/com/example/e_cynic/adapter/RedeemListAdapter.java
+++ app/src/main/java/com/example/e_cynic/adapter/RedeemListAdapter.java
@@ -1,45 +1,78 @@
 package com.example.e_cynic.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.e_cynic.R;
+import com.example.e_cynic.entity.Voucher;
+
+import java.util.List;
+
 public class RedeemListAdapter extends RecyclerView.Adapter<RedeemListAdapter.MyViewHolder>
 {
     Context context;
+    List<Voucher> voucherList;
 
-    public RedeemListAdapter(@NonNull Context context)
+    public RedeemListAdapter(Context context, List<Voucher> voucherList)
     {
-
+        this.context = context;
+        this.voucherList = voucherList;
     }
 
     @NonNull
     @Override
     public RedeemListAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.voucher_list,parent,false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RedeemListAdapter.MyViewHolder holder, int position)
     {
+        holder.voucherImgList.setImageResource(voucherList.get(position).voucherImage);
+        holder.voucherNameList.setText(voucherList.get(position).voucherName);
+        holder.voucherPointList.setText(String.valueOf(voucherList.get(position).voucherPoints));
 
+        holder.redeemBtnList.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                //TODO -> link to redeem history page
+            }
+        });
     }
 
     @Override
     public int getItemCount()
     {
-        return 0;
+        return voucherList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder
     {
+        ImageView voucherImgList;
+        TextView voucherNameList,voucherPointList;
+        Button redeemBtnList;
+
         public MyViewHolder(@NonNull View itemView)
         {
             super(itemView);
+
+            voucherImgList = itemView.findViewById(R.id.voucherImgList);
+            voucherNameList = itemView.findViewById(R.id.voucherNameList);
+            voucherPointList = itemView.findViewById(R.id.voucherPointList);
+            redeemBtnList = itemView.findViewById(R.id.redeemBtnList);
         }
     }
 }
